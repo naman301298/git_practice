d6d224d458953d3df6bdcd02b443437ed6d09bbd
naman <namanchoudhary114@gmail.com>
2026-07-20 00:06:13 +0530

    stash commit

--- one.js
+++ one.js
@@ -2,3 +2,11 @@ console.log("Hello second file ");
 
 
 //2025 is coming 
+
+
+//  git stash    
+//   git stash list    
+//   git stash pop 
+//   git stash drop
+//   git stash apply
+//   git stash clear
